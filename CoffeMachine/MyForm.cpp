0d4b8e1bc0ccdac9ddcc5d159669fa6f2de11496
Kamil Kopyra <kamilkopyra@gmail.com>
2026-04-21 14:15:15 +0200

    add .vs to gitignore

---
+++ CoffeMachine/MyForm.cpp
@@ -0,0 +1,14 @@
+#include "MainWindow.h"
+
+using namespace System;
+using namespace System::Windows::Forms;
+
+[STAThreadAttribute]
+
+int main(array<String^>^ args) {
+    Application::EnableVisualStyles();
+    Application::SetCompatibleTextRenderingDefault(false);
+    CoffeMachine::MyForm form;
+    Application::Run(% form);
+    return 0;
+}
