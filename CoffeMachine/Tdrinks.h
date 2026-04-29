487195f35395323825571abd263a68b7338c8afd
Filip Materowski <filip.materowski@gmail.com>
2026-04-29 10:10:29 +0200

    Dodanie historii kawy

--- CoffeMachine/Tdrinks.h
+++ CoffeMachine/Tdrinks.h
@@ -2,15 +2,24 @@
 #include <iostream>
 #include <string>
 
+// Czym jest kawa?
+/* 
+Historia kawy rozpoczęła się w Etiopii (rejon Kaffa), gdzie według legendy pobudzające właściwości owoców kawowca odkrył pasterz,
+obserwując ożywione kozy. Stamtąd trafiła do Jemenu (XV/XVI w.), zyskując popularność w świecie arabskim, a w XVII w. dotarła do Europy.
+Od tego czasu nasze życie nie wyglądało już tak samo...
+*/
+
 class Tdrinks
 {
 	std::string name;
-	int volume;
-	int volumeOfMilk;
+	float volume;
+	float volumeOfMilk;
+	int power;  // Power = (1,2,3,4,5) default = 3 
 public:
-	Tdrinks(std::string name, int volume, int volumeofMilk);
+	Tdrinks(std::string name, int volume, int volumeofMilk, int power);
 	std::string getName();
-	int getVolume();
+	int getPowerOfCoffe();
 	bool hasMilk();
+	int getVolume();
 	int getVolumeOfMilk();
 };
