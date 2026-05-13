2f06ea084626579081c7889c4ca4f72799dc3360
Filip Materowski <filip.materowski@gmail.com>
2026-05-13 09:02:19 +0200

    Przniesiono Zmiany z CoffeMachine do Wersji Terminalowej

--- CoffeMachine/CoffeMachine.cpp
+++ CoffeMachine/CoffeMachine.cpp
@@ -9,7 +9,11 @@ void CoffeMachine::updateMachineStatus() {
 	hasMilk = milk.isEnough();
 	hasWater = water.isEnough();
 	hasBeans = beans.isEnough();
-
+	
+	if (!hasMilk) std::cout << "Dolej mleka\n";
+	else if (!hasWater) std::cout << "Dolej wode\n";
+	else if (!hasBeans) std::cout << "Dosyp ziarna\n";
+	else std::cout << "Wystarczające składniki\n";
 }
 
 
@@ -24,7 +28,41 @@ void CoffeMachine::initializeMachine()
 	cupsServed = 0;
 }
 
-void CoffeMachine::cleanMachine() {
+void CoffeMachine::cleanMachine() 
+{
 	isClean = true;
 	history.push_back("Machine cleaned");
 }
+
+void CoffeMachine::addWater(int amount) 
+{
+	water.refill(amount);
+	updateMachineStatus();
+	history.push_back("Added " + std::to_string(amount) + "ml of water");
+}
+
+void CoffeMachine::addBeans(int amount) 
+{
+	beans.refill(amount);
+	updateMachineStatus();
+	history.push_back("Added " + std::to_string(amount) + "g of beans");
+}
+
+void CoffeMachine::addMilk(int amount) 
+{
+	milk.refill(amount);
+	updateMachineStatus();
+	history.push_back("Added " + std::to_string(amount) + "ml of milk");
+}
+
+void CoffeMachine::descaling() 
+{
+	isOperational = true;
+	history.push_back("Machine descaled");
+}
+
+bool CoffeMachine::checkIngredientsFor(Tdrinks drink) 
+{
+	return water.getCapacity() >= drink.getVolume() && milk.getCapacity() >= drink.getVolumeOfMilk() && beans.getCapacity() >= drink.getAmountOfCoffee();
+}
+
