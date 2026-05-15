c33096d6882a098b0ad066de0d4de8cf9a4996f2
Filip Materowski <filip.materowski@gmail.com>
2026-05-15 12:54:19 +0200

    duck know :/

---
+++ CoffeMachine/MainWindow.h
@@ -0,0 +1,25 @@
+#pragma once
+#include <QMainWindow>
+#include "Ingredient.h"   
+
+// Ten plik generuje CMake automatycznie z MainWindow.ui
+// Nazwa: ui_MainWindow.h, prefix "Ui::"
+QT_BEGIN_NAMESPACE
+namespace Ui { class MainWindow; }
+QT_END_NAMESPACE
+
+class MainWindow : public QMainWindow {
+    Q_OBJECT  // ← WYMAGANE dla sygnałów i slotów
+
+public:
+    explicit MainWindow(QWidget* parent = nullptr);
+    ~MainWindow();
+
+private slots:
+    void onAddClicked();      // slot dla przycisku "Dodaj"
+    void onSubtractClicked(); // slot dla przycisku "Odejmij"
+
+private:
+    Ui::MainWindow* ui;       // ← wskaźnik do wygenerowanego UI
+    Calculator calculator;    // ← instancja Twojej klasy logiki
+};
