7af3698f2811639c077b13209d57c41e8af7f631
Kamil Kopyra <kamilkopyra@gmail.com>
2026-06-07 22:38:05 +0200

    nwm nie pamiętam

--- Qt_CoffeMachine/mainwindow.cpp
+++ Qt_CoffeMachine/mainwindow.cpp
@@ -66,37 +66,31 @@ void MainWindow::on_pushButton_2_clicked()
 void MainWindow::on_Settings_button_clicked()
 {
     QMenu *menu = new QMenu(this);
-
-    menu->addAction("Status");
-    menu->addAction("Dodaj składniki");
+    // menu->addAction("Status");
+    // menu->addAction("Dodaj składniki");
+    // menu->addSeparator();
     menu->addAction("Motyw");
     menu->addAction("Język");
-    menu->addSeparator();
-    menu->addAction("Statystyka");
 
     connect(menu, &QMenu::triggered, this, [this](QAction *action) {
-
         qDebug() << "Wybrano: " << action -> text();
-
-            // if (action->text() == "Dodaj składniki") {
-            //     // AddIngredientsMenu *dialog = new AddIngredientsMenu(this);
-            //     // dialog->setWindowFlags(Qt::Window);
-            //     // dialog->show();
-            //     ind_menu->show();
-            //     ind_menu->raise();
-            //     ind_menu->activateWindow();
-            // }
-            // if (action->text() == "Status") {
-            //     // statusMenu *dialog = new statusMenu(this);
-            //     // dialog->setWindowFlags(Qt::Window);
-            //     // dialog->show();
-            //     stat_menu->show();
-            //     stat_menu->raise();
-            //     stat_menu->activateWindow();
-            // }
-        });
-
+        // if (action->text() == "Dodaj składniki") {
+        //     // AddIngredientsMenu *dialog = new AddIngredientsMenu(this);
+        //     // dialog->setWindowFlags(Qt::Window);
+        //     // dialog->show();
+        //     ind_menu->show();
+        //     ind_menu->raise();
+        //     ind_menu->activateWindow();
+        // }
+        // if (action->text() == "Status") {
+        //     // statusMenu *dialog = new statusMenu(this);
+        //     // dialog->setWindowFlags(Qt::Window);
+        //     // dialog->show();
+        //     stat_menu->show();
+        //     stat_menu->raise();
+        //     stat_menu->activateWindow();
+        // }
+    });
     menu->exec(ui->Settings_button->mapToGlobal(
         ui->Settings_button->rect().bottomLeft()));
 }
-
--- Qt_CoffeMachine/menuwindow.cpp
+++ Qt_CoffeMachine/menuwindow.cpp
@@ -9,6 +9,7 @@
 #include "statusmenu.h"
 #include <QMenu>
 #include <QMessageBox>
+#include "servicewindow.h"
 
 int currentCoffeId=0;
 int checkBoxId=0;
@@ -336,6 +337,13 @@ void MenuWindow::on_settings_button_clicked()
             stat_menu->raise();
             stat_menu->activateWindow();
         }
+        if (action->text() == "Serwis") {
+            serviceWindow *dialog = new serviceWindow(this);
+            dialog->setWindowFlags(Qt::Window);
+            dialog->show();
+        }
+
+
     });
 
     menu->exec(ui->settings_button->mapToGlobal(
